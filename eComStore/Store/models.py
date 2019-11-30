ea3281f41fbe3cec3c030d70654c3511fcab546e
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-12-01 03:46:06 +0500

    eCom Store

--- eComStore/Store/models.py
+++ eComStore/Store/models.py
@@ -4,13 +4,23 @@ from django.db import models
 
 
 class Product(models.Model):
+        ''' 
+                Products Model
+        '''
         product_id = models.AutoField
         product_name = models.CharField(max_length=50, verbose_name='Product Name')
-        product_desc = models.CharField(max_length = 500, verbose_name='Product Description')
+        product_category = models.CharField(max_length=50, default="")
+        product_subcategory = models.CharField(max_length=50)
+        product_price = models.IntegerField()
+        product_desc = models.TextField(max_length = 500, verbose_name='Product Description')
         pub_date = models.DateTimeField(auto_now_add=True)
+        product_image = models.ImageField(upload_to='products')
 
         class Meta:
                 verbose_name_plural = 'Product'
         
         def __str__(self):
                 return self.product_name
+        
+        def product_desc_sum(self):
+                return self.product_desc[:40]
--- eComStore/Store/views.py
+++ eComStore/Store/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+from math import ceil
 
 
 def index(request):
         url = 'Store/index.html'
-        return render(request, url)
+        products = Product.objects.all()
+        # n = len(products)
+        # no_slides = n//4 + ceil((n/4)-(n//4))
+
+        # all_products = [[products, range(1, no_slides), no_slides], [products, range(1, no_slides), no_slides]]
+
+        all_products = []
+        catprods = Product.objects.values('product_category', 'id')
+        cats = {item['product_category'] for item in catprods}
+
+        for cat in cats:
+                prod = Product.objects.filter(product_category = cat)
+                n = len(prod)
+                no_slides = n//4 + ceil((n/4)-(n//4)) 
+                all_products.append([prod, range(1, no_slides), no_slides])
+
+        params = { 'product': products, 'all_product': all_products}
+        return render(request, url, params)
 
 
 def about(request):
-        return HttpResponse("<h2>About Page</h2>")
+        url = 'Store/about.html'
+        return render(request, url)
 
 
 def contact(request):
