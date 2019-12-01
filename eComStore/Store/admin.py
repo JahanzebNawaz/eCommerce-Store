3d7e178098c9af38aad0c8906d248baef3027da1
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-12-02 02:05:20 +0500

    eCom Store

--- eComStore/Store/admin.py
+++ eComStore/Store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ from .models import Product, Contact, Orders
 admin.site.register(Product)
 admin.site.register(Contact)
 admin.site.register(Orders)
+admin.site.register(OrderUpdate)
--- eComStore/Store/views.py
+++ eComStore/Store/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 from .forms import ContactForm
 # Create your views here.
 from math import ceil
+import json
 
 
 def index(request):
     url = 'Store/index.html'
-    products = Product.objects.all()
+    try:
+        products = Product.objects.all()
+    except Product.DoestNotExist:
+        return HttpResponse('Error fetching data from database')
 
     all_products = []
     catprods = Product.objects.values('product_category', 'id')
@@ -17,6 +21,37 @@ def index(request):
 
     for cat in cats:
         prod = Product.objects.filter(product_category=cat)
+        n = len(prod)
+        no_slides = n//4 + ceil((n/4)-(n//4))
+        all_products.append([prod, range(1, no_slides), no_slides])
+
+    params = {'product': products, 'all_product': all_products}
+    return render(request, url, params)
+
+
+def search_match(query, item):
+    ''' match query of user with items in database '''
+    if query in item.desc.lower() or query in item.product_name.lower():
+        return True
+    else:
+        return False
+
+
+def search(request):
+    url = 'Store/index.html'
+    query = request.GET.get('search')
+    try:
+        products = Product.objects.all()
+    except Product.DoestNotExist:
+        return HttpResponse('Error fetching data from database')
+
+    all_products = []
+    catprods = Product.objects.values('product_category', 'id')
+    cats = {item['product_category'] for item in catprods}
+
+    for cat in cats:
+        prodtemp = Product.objects.filter(product_category=cat)
+        prod = [item for item in prodtemp if search_match(query, item)]
         n = len(prod)
         no_slides = n//4 + ceil((n/4)-(n//4))
         all_products.append([prod, range(1, no_slides), no_slides])
@@ -37,7 +72,10 @@ def contact(request):
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
         desc = request.POST.get('desc', '')
-        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        try:
+            contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        except Contact.DoesNotExist:
+            return redirect('contact')
 
         if contact:
             contact.save()
@@ -51,20 +89,35 @@ def contact(request):
 
 
 def tracker(request):
+    if request.method == "POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append(
+                        {'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(
+                        [updates, order[0].items_json], default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     url = 'Store/tracker.html'
-
-    return render(request, url)
-
-
-def search(request):
-    url = 'Store/search.html'
     return render(request, url)
 
 
 def product(request, g_id):
     ''' fetch product using the id'''
-    product = Product.objects.filter(id=g_id)
-
+    try:
+        product = Product.objects.filter(id=g_id)
+    except Product.DoesNotExist:
+        return redirect('index')
     url = 'Store/productview.html'
     return render(request, url, {'product': product[0]})
 
@@ -72,6 +125,7 @@ def product(request, g_id):
 def checkout(request):
     if request.method == "POST":
         items_json = request.POST.get('itemsJson', '')
+        amount = request.POST.get('amount', '')
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         address = request.POST.get('address', '')
@@ -79,10 +133,22 @@ def checkout(request):
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
-        order = Orders(items_json=items_json, name=name, email=email,
-                       address=address, city=city, state=state,
-                       zip_code=zip_code, phone=phone)
-        order.save()
+
+        try:
+            order = Orders(items_json=items_json, name=name, email=email,
+                           address=address, city=city, state=state,
+                           zip_code=zip_code, phone=phone, amount=amount)
+            if order:
+                order.save()
+                messages.success(
+                    request, 'Successfully Order submitted! ')
+            else:
+                messages.error(request, 'Error submitting  Order!')
+                return redirect('index')
+
+        except Orders.DoesNotExist:
+            return redirect('checkout')
+
         thank = True
         id = order.order_id
         return render(request, 'Store/checkout.html', {'thank': thank,
